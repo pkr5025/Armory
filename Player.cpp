b66fa1378442971187d580611215382de2d657b3
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2018-12-15 00:46:22 -0500

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,31 @@
+
+#include "Player.h"
+#include <iostream>
+#include <algorithm>
+
+void player::getinitialhand(deck &d)
+{
+	for (int i = 0; i < 7; i++) {
+		hand.push_back(d.draw());
+	}
+}
+
+card player::playcard(card c) {
+
+	auto a = std::find(hand.begin(), hand.end(), c);
+	if (a != hand.end()) // c is in vector
+	{
+		hand.erase(a);
+		return c;
+	}
+	else {
+		throw std::logic_error("Trying to play a card you don't have you CHEATER!");
+	}
+}
+void player::displayscore() {
+	std::cout << score << std::endl;
+}
+
+int player::getscore() {
+	return score;
+}
---
+++ Player.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include "Deck.h"
+
+class player {
+public:
+	void getinitialhand(deck &d);  //generates a 7 card opening hand
+	card playcard(card c);
+	void displayscore();
+	int getscore();
+
+
+private:
+	std::vector<card> hand;
+	int score = 0;
+};
+
