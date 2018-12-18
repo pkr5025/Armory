462a7adcb078a9eda6b572196c236e4d9bfcdfc3
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2018-12-18 00:09:21 -0500

    Added playcard capability and human player class

--- Player.cpp
+++ Player.cpp
@@ -2,6 +2,9 @@
 #include "Player.h"
 #include <iostream>
 #include <algorithm>
+#include <string>
+
+// BASE PLAYER FUNCTIONS
 
 void player::getinitialhand(deck &d)
 {
@@ -10,18 +13,7 @@ void player::getinitialhand(deck &d)
 	}
 }
 
-card player::playcard(card c) {
 
-	auto a = std::find(hand.begin(), hand.end(), c);
-	if (a != hand.end()) // c is in vector
-	{
-		hand.erase(a);
-		return c;
-	}
-	else {
-		throw std::logic_error("Trying to play a card you don't have you CHEATER!");
-	}
-}
 void player::displayhand() {
 	for (auto i : hand) {
 		i.displaycard();
@@ -34,3 +26,67 @@ void player::displayscore() {
 int player::getscore() {
 	return score;
 }
+
+
+///////////// HUMAN PLAYER FUNCTIONS //////////////////////////
+
+card humanplayer::playcard() {
+	std::cout << "Choose a card to play. Enter the suit first as (h,d,s, or c):"<<std::endl;
+	char s;
+	std::cin >> s;
+	std::string n;
+	std::cout << "Enter the card value as a,k,j, or it's numerical value" << std::endl;
+
+	std::cin >> n;
+
+	suit cardsuit;
+	value cardvalue;
+
+	switch (s) {
+	case('h'):
+	case('H'):
+		cardsuit = Hearts; break;
+	case('d'):
+	case('D'):
+		cardsuit = Diamonds; break;
+	case('s'):
+	case('S'):
+		cardsuit = Spades; break;
+	case('c'):
+	case('C'):
+		cardsuit = Clubs; break;
+	default:
+		throw std::logic_error("Not a valid suit"); break;
+	}
+	if (n == "a")
+		cardvalue = Ace;
+	else if (n == "k")
+		cardvalue = King;
+	else if (n == "j")
+		cardvalue = Jack;
+	else {
+		int i = std::stoi(n);
+		if (1 < i < 11)
+			cardvalue = static_cast<value> (i);
+		else
+			throw std::logic_error("Not a valid value");
+	}
+	card c(cardsuit, cardvalue);
+
+
+	auto a = std::find(hand.begin(), hand.end(), c);
+	if (a != hand.end()) // c is in vector
+	{
+		hand.erase(a);
+		std::cout << "You play: ";
+		c.displaycard();
+		return c;
+	}
+	else { 
+		std::cout << "Trying to play a card you don't have you CHEATER!" << std::endl;
+		throw std::logic_error("Trying to play a card you don't have you CHEATER!");
+	}
+}
+
+
+// COMPUTER PLAYER FUNCTIONS
